cb263f867d3cc782291f2b8000b98e4143801590
Elifnur <elifxnur118@gmail.com>
2025-02-07 17:40:23 +0300

    gradle version updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,24 @@ import 'package:music_player/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home()
+    home: SandBox()
   ));
 }
+ class SandBox extends StatelessWidget {
+   const SandBox({super.key});
+ 
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: AppBar(
+         title:  const Text('Scaffold',
+             style: TextStyle (color:Colors.white)),
+         backgroundColor: Colors.purpleAccent,
+       ),
+       body:Column(children: [],),
 
+     );
+   }
+ }
+ 
 
